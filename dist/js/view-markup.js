264039b987b487504fab83e277deff7991a5becd
Marshall Crosby <marshall@blendinteractive.com>
2022-06-08 10:43:25 -0500

    Version update

--- dist/js/view-markup.js
+++ dist/js/view-markup.js
@@ -1,5 +1,5 @@
 /*!
-    * View markup v1.1.6
+    * View markup v1.2.0
     * Plugin that makes it easy for developers to view and copy the html needed for a component.
     *
     * Copyright 2021-2022 Marshall Crosby
